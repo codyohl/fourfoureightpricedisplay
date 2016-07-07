be3eae77f4d925699c4dac49f662230b9843986c
Cody Ohlsen <t-codyo@microsoft.com>
2016-07-07 10:32:37 -0700

    Bug fix

--- sketch_jul06a.ino
+++ sketch_jul06a.ino
@@ -9,6 +9,7 @@ void setup() {
   pinMode(CLOCK_2, OUTPUT);
   pinMode(DATA_1, OUTPUT);
   pinMode(DATA_2, OUTPUT);
+  // Serial.begin(9600);
 }
 
 void loop()
@@ -17,7 +18,7 @@ void loop()
   //               0,X,1,2,3,4,5,6,7,X,X,X,8,9,A,B,X,X,X,X,C,D,E,F,0,1,X,2,X,X,3,4,5,6,7,X
   int cycle[36] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
   int segments[] = { 10,17,20,26,32,38,44,51,56,64,0,1,2,3,5,6,-1,3,5,-1,2,3,4,0,6,-1,2,3,4,5,6,-1,1,4,3,5,6,-1,2,1,4,5,6,-1,2,1,0,6,5,4,-1,1,2,3,5,-1,1,2,3,4,0,6,5,-1,1,2,3,4,5,6,-1 };
-  int pins[] = { 2,3,4,5,6,7,8,12,13,14,15,20,21,22,23,24,25,27,30,31,32 };
+  int pins[] = { 2,3,4,5,6,7,8,13,14,15,20,21,22,23,24,25,27,30,31,32,33 };
 
   int row = 2;
   int display = 236;
@@ -35,22 +36,23 @@ void loop()
     cycle[i] = (i == 0) ? 1 : 0;
   }
 
-  int remainder = display
+  int remainder = display;
 
-  for (int digitIndex = 2; digitIndex >= 0; digitIndex++)
+  for (int digitIndex = 2; digitIndex >= 0; digitIndex--)
   {
-  	  int digit = remainder % 10;
-  	  remainder -= digit;
-  	  remainder /= 10;
-	  int index = segments[lsb];
-	  while (segments[index] != -1)
-	  {
-	    int segmentNumber = segment[index];
-	    int pinIndex = digitIndex * 7 + segmentNumber;
-	    int pinNumber = pins[pinIndex];
-	    cycle[pinNumber] = 1;
-	    index++;
-	  }
+      int digit = remainder % 10;
+      remainder -= digit;
+      remainder /= 10;
+    int index = segments[digit];
+    while (segments[index] != -1)
+    {
+      int segmentNumber = segments[index];
+      int pinIndex = digitIndex * 7 + segmentNumber;
+      int pinNumber = pins[pinIndex];
+      cycle[pinNumber] = 1;
+      index++;
+      // Serial.println(pinNumber);
+    }
   }
         
   for (int i = 0; i < 36; i++)
@@ -61,5 +63,4 @@ void loop()
     delay(1);
     digitalWrite(CLOCK_1, HIGH);
   }
-  }
-} 
+}
